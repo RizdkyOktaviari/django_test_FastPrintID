1dc9542badc524696112ed04ee14a87e7d39212c
Rizdky Oktaviari <kudaberkuda@gmail.com>
2023-10-04 09:02:24 +0700

    selesai fitur create read

---
+++ test_programmer/produk/ProdukForm.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Produk, Kategori, Status
+
+class ProdukForm(forms.ModelForm):
+    class Meta:
+        model = Produk
+        fields = ['nama_produk', 'harga', 'kategori', 'status']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['kategori'].choices = [(k.id, k.nama_kategori) for k in Kategori.objects.all()]
+        self.fields['status'].choices = [(s.id, s.nama_status) for s in Status.objects.all()]
+
+class KategoriForm(forms.ModelForm):
+    class Meta:
+        model = Kategori
+        fields = ['nama_kategori']
+
+class StatusForm(forms.ModelForm):
+    class Meta:
+        model = Status
+        fields = ['nama_status']
+
--- test_programmer/produk/views.py
+++ test_programmer/produk/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from produk.models import Produk
 from produk.models import Kategori
 from produk.models import Status
+from produk.ProdukForm import ProdukForm
 
 class Index(ListView):
     queryset = Produk.objects.all()
@@ -12,7 +13,6 @@ class Index(ListView):
     
 
 class AddProduk(CreateView):
-    model = Produk
-    fields = ['nama_produk', 'harga', 'kategori', 'status']
+    form_class = ProdukForm
     template_name = 'produk/add.html'
     success_url = reverse_lazy('produk:index')
