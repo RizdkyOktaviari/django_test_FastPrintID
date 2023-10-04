73d07b3b64334a275ca667eb4f3d581e5cca8906
Rizdky Oktaviari <kudaberkuda@gmail.com>
2023-10-04 11:30:42 +0700

    selesai crud semua

--- test_programmer/produk/urls.py
+++ test_programmer/produk/urls.py
@@ -5,6 +5,20 @@ from . import views
 app_name = 'produk'
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
-    path('add/', views.AddProduk.as_view(), name='add'),
+    path('', views.Home.as_view(), name='home'),
+    # Kategori
+    path('index_kategori/', views.IndexKategori.as_view(), name='index_kategori'),
+    path('add_kategori/', views.AddKategori.as_view(), name='add_kategori'),
+    path('edit_kategori/<pk>', views.EditKategori.as_view(), name='edit_kategori'),
+    path('delete_kategori/<int:kategori_id>/', views.delete_kategori, name='delete_kategori'),
+    # Produk
+    path('index_produk/', views.Index.as_view(), name='index_produk'),
+    path('add_produk/', views.AddProduk.as_view(), name='add_produk'),
+    path('edit_produk/<pk>', views.EditProduk.as_view(), name='edit_produk'),
+    path('delete_produk/<int:produk_id>/', views.delete_produk, name='delete_produk'),
+    # Status
+    path('index_status/', views.IndexStatus.as_view(), name='index_status'),
+    path('add_status/', views.AddStatus.as_view(), name='add_status'),
+    path('edit_status/<pk>', views.EditStatus.as_view(), name='edit_status'),
+    path('delete_status/<int:status_id>/', views.delete_status, name='delete_status'),
 ]
--- test_programmer/produk/views.py
+++ test_programmer/produk/views.py
@@ -5,14 +5,72 @@ from django.urls import reverse_lazy
 from produk.models import Produk
 from produk.models import Kategori
 from produk.models import Status
-from produk.ProdukForm import ProdukForm
+from produk.ProdukForm import ProdukForm, KategoriForm, StatusForm
+from django.shortcuts import render, redirect
 
+class Home(ListView):
+    queryset = Produk.objects.all()
+    template_name = 'layouts/index.html'
+
+# view Produk
 class Index(ListView):
     queryset = Produk.objects.all()
-    template_name = 'produk/index.html'
-    
+    template_name = 'produk/index.html' 
 
 class AddProduk(CreateView):
     form_class = ProdukForm
     template_name = 'produk/add.html'
-    success_url = reverse_lazy('produk:index')
+    success_url = reverse_lazy('produk:index_produk')
+
+class EditProduk(UpdateView):
+    model = Produk
+    form_class = ProdukForm
+    template_name = 'produk/edit.html'
+    success_url = reverse_lazy('produk:index_produk')
+
+def delete_produk(request, produk_id):
+    produk = Produk.objects.get(id=produk_id)
+    produk.delete()
+    return redirect('produk:index_produk')
+
+# view Kategori
+class IndexKategori(ListView):
+    queryset = Kategori.objects.all()
+    template_name = 'kategori/index.html'
+
+class AddKategori(CreateView):
+    form_class = KategoriForm
+    template_name = 'kategori/add.html'
+    success_url = reverse_lazy('produk:index_kategori')
+
+class EditKategori(UpdateView):
+    model = Kategori
+    form_class = KategoriForm
+    template_name = 'kategori/edit.html'
+    success_url = reverse_lazy('produk:index_kategori')
+
+def delete_kategori(request, kategori_id):
+    kategori = Kategori.objects.get(id=kategori_id)
+    kategori.delete()
+    return redirect('produk:index_kategori')
+
+# view Status
+class IndexStatus(ListView):
+    queryset = Status.objects.all()
+    template_name = 'status/index.html'
+
+class AddStatus(CreateView):
+    form_class = StatusForm
+    template_name = 'status/add.html'
+    success_url = reverse_lazy('produk:index_status')
+
+class EditStatus(UpdateView):
+    model = Status
+    form_class = StatusForm
+    template_name = 'status/edit.html'
+    success_url = reverse_lazy('produk:index_status')
+
+def delete_status(request, status_id):
+    status = Status.objects.get(id=status_id)
+    status.delete()
+    return redirect('produk:index_status')
